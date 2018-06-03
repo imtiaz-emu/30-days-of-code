ff6ae1ed5fb82cb4bc96768d8fc017f8588d28bc
imtiaz-emu <imtiaz.emu.it@gmail.com>
2018-06-03 19:58:31 +0600

    Day 11: 2D Arrays

---
+++ day11.py
@@ -0,0 +1,28 @@
+'''
+Problem Statement: https://www.hackerrank.com/challenges/30-2d-arrays/problem
+Author: Imtiaz Emu
+Language: Python 3
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+  arr = []
+  maxResult = -10000
+  
+  for _ in range(6):
+    arr.append(list(map(int, input().rstrip().split())))
+  
+  for i in range(4):
+    for j in range(4):
+      hourSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + \
+                arr[i + 1][j + 1] + \
+                arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
+      maxResult = max(maxResult, hourSum)
+  
+  print(maxResult)
